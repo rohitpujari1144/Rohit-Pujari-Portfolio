09ee34128e442af087f7876c487face309b288fa
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-07-24 07:53:16 +0530

    Add files via upload

---
+++ src/Components/Copyright/Copyright.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './copyright.css'
+
+function Copyright() {
+    return (
+        <>
+            <div className='copyrightDiv'>
+                <h4 className='copyright'><i class="fa-regular fa-copyright"></i> 2023 Rohit</h4>
+            </div>
+        </>
+    )
+}
+
+export default Copyright
